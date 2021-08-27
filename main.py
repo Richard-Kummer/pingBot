e54f9ee9229179cb0bac1607403a2fda978841a9
Richard Kummer <richardkummer8@gmail.com>
2021-08-27 16:53:47 -0400

    Finished role label commands

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 
 from discord.ext import commands
+from discord import AllowedMentions
 from dotenv import load_dotenv
 
 registered_roles = {}
@@ -11,6 +12,8 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 bot = commands.Bot(command_prefix='+', help_command=None)
 
+bot.allowed_mentions = AllowedMentions.none()
+
 
 @bot.event
 async def on_ready():
@@ -19,11 +22,11 @@ async def on_ready():
 
 @bot.command(name='help')
 async def help_command(ctx: commands.Context):
-    await ctx.send("""```
+    await ctx.reply("""```
 +help                              - Shows this message
 +invite                            - Shows a bot invite link to add to your server
 +role
-    set <role_name> <@role>        - Defines a new role, where custom mention perms can be set
+    set <role_name> <@role>        - Adds role label, where custom mention perms can be set
     remove <role_name>             - Removes a role from being pinged entirely via this bot
     list                           - Lists all roles registered via this bot interface
 +roleperms
@@ -35,13 +38,51 @@ async def help_command(ctx: commands.Context):
 
 @bot.command(name='invite')
 async def invite_command(ctx: commands.Context):
-    await ctx.send("https://discord.com/api/oauth2/authorize?client_id=880871977382998086&permissions=470080&scope=bot")
+    await ctx.reply("https://discord.com/api/oauth2/authorize?client_id=880871977382998086&permissions=470080&scope=bot")
 
 
 @bot.command(name='role')
 @commands.has_permissions(manage_roles=True, mention_everyone=True)
 async def role_command(ctx: commands.Context, subcommand: str, *args):
-    pass
+    if subcommand == "set":
+        role_name = args[0]
+
+        try:
+            role = await commands.RoleConverter().convert(ctx, args[1])
+
+            registered_roles[role_name] = role
+            if role_name not in role_perms:
+                role_perms[role_name] = []
+
+            await ctx.reply(f"Added role label `{role_name}` => <@&{role.id}>")
+
+        except commands.errors.RoleNotFound:
+            await ctx.reply(f"Could not find role \"{args[1]}\"")
+
+    elif subcommand == "remove":
+        role_name = args[0]
+
+        try:
+            role = registered_roles[role_name]
+
+            del registered_roles[role_name]
+            del role_perms[role_name]
+
+            await ctx.reply(f"Removed role label `{role_name}` => <@&{role.id}>")
+        except KeyError:
+            await ctx.reply(f"Role label `{role_name}` does not exist")
+
+    elif subcommand == "list":
+        roles_list = ""
+
+        if registered_roles:
+            for role_name in registered_roles:
+                roles_list += f"`{role_name}` => <@&{registered_roles[role_name].id}>\n"
+
+            await ctx.reply(roles_list)
+
+        else:
+            await ctx.reply("There are no roles currently registered with this bot.")
 
 
 @bot.command(name='roleperms')
